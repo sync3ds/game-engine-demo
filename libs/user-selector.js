2cdc40ff267b50e6f55770b52a1e77d0f691978b
dario.corbelli <info@dariocorbelli.it>
2020-10-29 17:06:54 +0100

    Added WASD control

--- libs/user-selector.js
+++ libs/user-selector.js
@@ -12,15 +12,22 @@ export class UserSelector {
 	submitForm(evt){
 		evt.preventDefault();
 		this.userSelection.username = document.getElementById('username').value;
-		let levelRadios = document.getElementsByName('level');
-		for (var i = 0; i < levelRadios.length; i++) {
-			if (levelRadios[i].checked) {
-				this.userSelection.place = levelRadios[i].value;
+		let mapRadios = document.getElementsByName('map');
+		for (var i = 0; i < mapRadios.length; i++) {
+			if (mapRadios[i].checked) {
+				this.userSelection.place = mapRadios[i].value;
+				break;
+			}
+		}
+		let characterRadios = document.getElementsByName('character');
+		for (var i = 0; i < characterRadios.length; i++) {
+			if (characterRadios[i].checked) {
+				this.userSelection.character = characterRadios[i].value;
 				break;
 			}
 		}
 
-		if(this.userSelection.username && this.userSelection.place){
+		if(this.userSelection.username && this.userSelection.place && this.userSelection.character){
 			this.initEngine();
 		} else {
 			alert("All fields are required.");
